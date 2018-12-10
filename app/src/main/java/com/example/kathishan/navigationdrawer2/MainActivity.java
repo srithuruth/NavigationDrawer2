53ead1dfee0980e06952df9a21a287cef00a8d85
Shri <usritharan@gmail.com>
2018-12-10 12:21:29 +0000

    Initial commit

---
+++ app/src/main/java/com/example/kathishan/navigationdrawer2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.kathishan.navigationdrawer2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+}
